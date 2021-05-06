8084d7f39abc4b8937696b4dd07e0c6a58a741e5
Jason Bacon <bacon@moray.acadix.biz>
2021-05-06 15:26:20 -0500

    Import Linux strlcpy and strlcat macros from biolibc

--- xtendc.h
+++ xtendc.h
@@ -1,3 +1,8 @@
 #define XC_CMD_MAX      4096
 
+#ifdef __linux__
+#define strlcpy(dest,src,len)   strcpy(dest,src)
+#define strlcat(dest,src,len)   strcat(dest,src)
+#endif
+
 #include "xtendc-protos.h"

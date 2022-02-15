14562d2d1a95ce2bc2efa4e6961eb038bc9164e8
Jason Bacon <bacon@NetBSD.org>
2022-02-14 19:37:01 -0600

    Add MacBook ffile run times

--- Fast-streamio/test.sh
+++ Fast-streamio/test.sh
@@ -4,7 +4,7 @@ file=~/Save/FreeBSD-13.0-RELEASE-amd64-disc1.iso
 for prog in iotest-low iotest-ffile iotest; do
     cc -o $prog $prog.c
     printf "$prog copying to copy.iso...\n"
-    time ./$prog $file copy.iso
+    /usr/bin/time ./$prog $file copy.iso
     cmp $file copy.iso
 done
 rm copy.iso

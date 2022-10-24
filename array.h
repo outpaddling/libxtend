70b16d8f09a29e6cd4ee6343e7ba83fe6971fe72
Jason W. Bacon <bacon4000@gmail.com>
2022-10-24 11:16:26 -0500

    array.h: Fix guard macro copied from mem.h (MEM -> ARRAY)

--- array.h
+++ array.h
@@ -1,5 +1,5 @@
-#ifndef _XTEND_MEM_H_
-#define _XTEND_MEM_H_
+#ifndef _XTEND_ARRAY_H_
+#define _XTEND_ARRAY_H_
 
 #ifndef _STDIO_H_
 #include <stdio.h>
@@ -8,4 +8,4 @@
 /* xt-shuffle.c */
 void *xt_shuffle(void *base, size_t nelem, size_t size);
 
-#endif // _XTEND_MEM_H_
+#endif // _XTEND_ARRAY_H_

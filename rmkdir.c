175b4ad3c8bd1ba68b3e36f0ec422c00ee237222
Jason W. Bacon <bacon4000@gmail.com>
2024-01-26 20:07:55 -0600

    xt_rmkdir():     Use local copy of path so it can replace '/' with '\0', even if     the argument in the caller is const.

--- rmkdir.c
+++ rmkdir.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>     // free()
 #include <sys/stat.h>
 #include <sys/types.h>
 #include "file.h"
@@ -34,33 +35,41 @@
 int     xt_rmkdir(const char *path, mode_t mode)
 
 {
-    char    *parent_end;
+    char    *parent,
+	    *parent_end;
+    struct stat st;
+    
+    if ( stat(path, &st) == 0 )
+	return 0;
     
     /* First try given path */
-    if ( mkdir(path,mode) == 0 )
+    if ( mkdir(path, mode) == 0 )
 	return 0;
     else
     {
 	/* Recursively attempt to make parent directories */
-	parent_end = strrchr(path,'/');
+	parent = strdup(path);            // path could be const in the caller
+	parent_end = strrchr(parent, '/');
 	if ( parent_end == NULL )
 	{
 	    /* Ran out of ancestors - give it up */
+	    free(parent);
 	    return -1;
 	}
 	else
 	{
 	    /* Try to make parent with recursive call */
 	    *parent_end = '\0';
-	    if ( xt_rmkdir(path,mode) == 0 )
+	    
+	    if ( xt_rmkdir(parent, mode) == 0 )
 	    {
 		/* If parent successfully made, try again to make current */
-		*parent_end = '/';
-		return mkdir(path,mode);
+		free(parent);
+		return mkdir(path, mode);
 	    }
 	    else
 	    {
-		*parent_end = '/';
+		free(parent);
 		return -1;
 	    }
 	}

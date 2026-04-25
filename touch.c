e15ed75540a9b7bef347b748b88a55951b61b6bf
Jason W. Bacon <bacon4000@gmail.com>
2026-04-25 16:11:46 -0500

    xt_touch(): New function similar to touch(1) command.

--- touch.c
+++ touch.c
@@ -1,5 +1,8 @@
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
+#include "file.h"
 
 /***************************************************************************
  *  Name:
@@ -22,13 +25,14 @@
  *
  *      XT_TOUCH_NO_CREATE - Do not create the file if it does not exist.
  *
- *      The mode argument represents permissions and other mode bits
- *      as described by chmod(1) and chmod(2);
+ *      The utimensat_flags argument is passed to utimensat(2) in order
+ *      to control handling of the path argument, symbolic links, etc.
+ *      See utimensat(2) for more information.
  *  
  *  Arguments:
- *      path    Absolute or relative pathname
- *      options Bitwise flags for controlling behavior
- *      mode    Permissions for the file
+ *      path            Absolute or relative pathname
+ *      options         Bitwise flags for controlling behavior
+ *      utimensat_flags Bitwise flags to pass to utimensat(2)
  *
  *  Returns:
  *      0 on success, non-zero error codes otherwise
@@ -37,27 +41,44 @@
  *      xt_touch("lockfile", 0, 0640);
  *
  *  See also:
- *      touch(1)
+ *      touch(1), utimensat(2)
  *
  *  History: 
  *  Date        Name        Modification
  *  2025-10-05  Jason Bacon Begin
  ***************************************************************************/
 
-int     xt_touch(const char *path, unsigned int options, unsigned int mode)
+int     xt_touch(const char *path, unsigned int options, int utimensat_flags)
 
 {
-    int     fd, open_flags;
+    int             fd, open_flags;
+    struct timespec times[2];
+    struct stat     st;
     
-    open_flags = O_WRONLY|O_CREAT|O_SYNC;
-    if ( ! (options & XT_TOUCH_NO_CREATE) )
-        open_flags |= O_CREAT;
-    
-    if ( (fd = open(path, open_flags, mode)) == 0 )
+    if ( stat(path, &st) == 0 )
     {
-        close(fd);
-        return EX_OK;
+        // tv_sec values are ignored, no need to set them
+        times[0].tv_nsec = times[1].tv_nsec = UTIME_OMIT;
+        if ( options & XT_TOUCH_UPDATE_ACCESS_TIME )
+            times[0].tv_nsec = UTIME_NOW;
+        if ( options & XT_TOUCH_UPDATE_MOD_TIME )
+            times[1].tv_nsec = UTIME_NOW;
+        if ( utimensat(AT_FDCWD, path, times, 0) == 0 )
+            return XT_OK;
+        else
+            return XT_FAIL;
     }
     else
-        return EX_CANTCREAT;
+    {
+        open_flags = O_WRONLY|O_CREAT|O_SYNC;
+        if ( ! (options & XT_TOUCH_NO_CREATE) )
+            open_flags |= O_CREAT;
+        if ( (fd = open(path, open_flags, 0777)) == 0 )
+        {
+            close(fd);
+            return XT_OK;
+        }
+        else
+            return XT_FAIL;
+    }
 }
